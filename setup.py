314bc0624f0ba5f31bd1e385ef137012d2eea2a1
gio <giovkast@gmail.com>
2021-06-19 12:38:52 +0200

    First Commit - Added Setup.py with configuration info'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django_editorjs_parser',
+    version='0.1',
+    description='Parser for blocks used by editor-js',
+    url='',
+    author='giovkast',
+    author_email='giovkast@gmail.com',
+    license='MIT',
+    packages=['django_editorjs_parser'],
+    zip_safe=False)
