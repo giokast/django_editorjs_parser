bf840e1e512e783a052cd5162ba7fb8b0afa8669
gio <giovkast@gmail.com>
2021-07-03 16:30:48 +0200

    Added firsts tests for parser input

---
+++ django_editorjs_parser/tests/test_django_js_parser.py
@@ -0,0 +1,15 @@
+import pytest
+
+from ..src.parser import 
+
+class Test_EditorJS_Parser: 
+
+    def test_parser_with_empty_list(self):
+        
+        assert False 
+
+    def test_parser_with_list(self):
+        assert False  
+
+    def test_parser_with_incorrect_input(self):
+        assert False 
